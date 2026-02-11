4c997191e7341b45f36f73b6787d9a43c821fd98
vworksofficial@gmail.com <vworksofficial@gmail.com>
2026-02-11 07:30:12 +0000

    sepertinya masih kurang lebar untuk padding kanan dan kirinya

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,8 @@ export default {
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
+        lg: '6rem',
+        xl: '10rem',
       },
       screens: {
         "2xl": "1400px",
