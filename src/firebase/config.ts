b4b72943c80eec239d6d9a06b284c41d38f8812f
vworksofficial <vworksofficial@gmail.com>
2026-02-25 06:28:53 +0000

    saya sudah mengaktifkan authentication dengan metode email/password, tap

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,8 +4,6 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Konfigurasi Firebase. Di lingkungan produksi Studio, nilai-nilai ini 
-// akan diambil dari environment variables.
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
@@ -15,7 +13,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
 };
 
-// Inisialisasi Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
